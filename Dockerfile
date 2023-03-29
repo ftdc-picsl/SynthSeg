50f6c5fb560be2f01ed0c02081bff7ad8c2f0925
Philip A Cook <cookpa@pennmedicine.upenn.edu>
2023-03-29 12:43:30 -0400

    ENH: Check bounding boxes and switch to CPU mode if brain doesn't fit in the specified bounding box

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,15 @@ FROM cookpa/synthseg:conda-0.1
 # we need
 COPY --from=antsx/ants:v2.4.3 /opt/ants /opt/ants
 
+# Base image includes sh, venv activation does not support that shell
+# Can't use dash because recognition of that depends on ps, which isn't installed in base image
+RUN rm /bin/sh && \
+    cd /bin && \
+    ln -s bash sh
+
+RUN . ${VIRTUAL_ENV}/bin/activate && \
+    pip install simpleitk==2.2.1
+
 COPY run_synthseg.py /opt/run_synthseg.py
 
 ENV LD_LIBRARY_PATH=/opt/ants/lib:$LD_LIBRARY_PATH
--- run_synthseg.py
+++ run_synthseg.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 import argparse
+import numpy as np
 import os
-from pathlib import Path
 import shutil
+import SimpleITK as sitk
 import subprocess
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -38,8 +39,11 @@ synthseg.add_argument("--vol", action='store_true', help="Output a CSV file cont
 synthseg.add_argument("--qc", action='store_true', help="Output a CSV file containing QC measures")
 synthseg.add_argument("--post", action='store_true', help="Output a multi-component image containing label posterior probabilities")
 synthseg.add_argument("--crop", help="Crop parameters, must be multiples of 32.", nargs='+', type=int, default = [224, 256, 192])
+synthseg.add_argument("--cpu", action='store_true', help="Use CPU instead of GPU, even if GPU is available")
 
 args = parser.parse_args()
+
+use_cpu = args.cpu
 
 input_t1w = os.path.realpath(args.input)
 output_prefix = os.path.realpath(args.output)
@@ -54,9 +58,39 @@ output_file_prefix = os.path.basename(output_prefix)
 # Crop input data if applicable
 synthseg_input = output_prefix + "SynthSegInput.nii.gz"
 
+synthseg_args = ['--i', synthseg_input, '--o', output_prefix + 'SynthSeg.nii.gz']
+
 if (args.mask is not None and os.path.isfile(args.mask)):
     print(f"Cropping input image around mask", flush=True)
     subprocess.run(['ExtractRegionFromImageByMask', '3', input_t1w, synthseg_input, args.mask, str(1), str(args.mask_pad)])
+
+    # Check if mask fits inside crop area
+    mask_image = sitk.ReadImage(args.mask)
+    label_shape = sitk.LabelShapeStatisticsImageFilter()
+    label_shape.Execute(sitk.Cast(mask_image, sitk.sitkUInt8))
+    # bb = [ xMin, yMin, zMin, xSize, ySize, zSize ]
+    bb_vox = label_shape.GetBoundingBox(1)
+
+    bb_min_vox = bb_vox[0:3]
+    bb_max_vox = tuple([int(b) for b in np.add(bb_vox[0:3], bb_vox[3:6])])
+
+    bb_min_mm = mask_image.TransformIndexToPhysicalPoint(bb_min_vox)
+    bb_max_mm = mask_image.TransformIndexToPhysicalPoint(bb_max_vox)
+
+    # The length of the sides of the bounding box in physical coordinates
+    # ITK uses LPS but lengths of the BB are the same as NIFTI RAS
+    bb_extent_ras = np.round(np.abs(np.subtract(bb_max_mm, bb_min_mm)))
+
+    crop_params = args.crop
+
+    for idx in range(0,3):
+        if bb_extent_ras[idx] > args.crop[idx]:
+            print("WARNING: brain mask extent is larger than cropped region for synthseg, " +
+                "using CPU to avoid running out of memory", flush=True)
+            # Crop must be a multiple of 32
+            crop_params[idx] = crop_params[idx] + 32
+            use_cpu = True
+
 else:
     shutil.copyfile(input_t1w, synthseg_input)
 
@@ -65,7 +99,7 @@ subprocess.run(['ResampleImage', '3', synthseg_input, synthseg_input, '1x1x1', '
 
 print(f"Input image: {input_t1w} resampled to {synthseg_input}")
 
-synthseg_args = ['--i', synthseg_input, '--o', output_prefix + 'SynthSeg.nii.gz', '--crop'] + [str(c) for c in args.crop]
+synthseg_args = synthseg_args + ['--crop'] + [str(c) for c in crop_params]
 
 # Set up synthseg options
 if (args.post):
@@ -78,10 +112,11 @@ if (args.vol):
     vol_output_file = output_prefix + 'Volumes.csv'
     synthseg_args = synthseg_args + ['--vol', vol_output_file]
 if (args.parc):
-    synthseg_args = synthseg_args + '--parc'
+    synthseg_args = synthseg_args + ['--parc']
 if (args.robust):
-    synthseg_args = synthseg_args + '--robust'
-
+    synthseg_args = synthseg_args + ['--robust']
+if (use_cpu):
+    synthseg_args = synthseg_args + ['--cpu']
 
 # Now call synthseg
 print(f"Running synthseg on {synthseg_input}", flush=True)
